0fdba144cca10847872db44dcbacfbcc41e8edc9
Ogre <theogreman@gmail.com>
2014-06-26 16:57:34 +0100

    Lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,10 +4,18 @@ from django.contrib import admin
 urlpatterns = patterns('',
 
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+
+    url(
+        regex=r'^lists/the-only-list-in-the-world/$',
+        view='lists.views.view_list',
         name='view_list',
     ),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+
+    url(
+        regex=r'^lists/new$',
+        view='lists.views.new_list',
+        name='new_list'
+    ),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
